7f1cb8f69b104a97ff0012860e670e2e52550ee2
Jawad Jahangir <jawadjahangir89@gmail.com>
2022-05-13 05:21:21 +0600

    data deletion page created

---
+++ client/pages/terms/data-deletion.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-responsive>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-card
+            color="transparent"
+            flat
+            tile
+            class="mt-8 mb-16"
+            style="margin-top: 100px"
+          >
+            <v-card-title class="mb-4">How to delete your data</v-card-title>
+            <v-card-text class="">
+              Please follow the instructions below step by step to remove your
+              data from this website:
+              <ul>
+                <li>Sign in to your iot snacksbox account</li>
+                <li>
+                  If you're not already in the dashboard page, then click on
+                  your name displayed on the top right corner of the screen on
+                  the navbar
+                </li>
+                <li>
+                  In the dropdown menu click on the Dashboard option and you'll
+                  be taken to the dashboard page
+                </li>
+                <li>
+                  On the left sidebar menu you'll find the Profile option.
+                  Clicking on it will give you the profile page
+                </li>
+                <li>
+                  In the profile page you can edit all of your profile data
+                </li>
+                <li>
+                  There are two sections in the profile page, each with its own
+                  edit button in the top right corner of the section
+                </li>
+                <li>
+                  In the edit form you can simply remove anything displayed in
+                  any of the form fields and save
+                </li>
+              </ul>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-responsive>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  computed: {},
+  created() {},
+  methods: {},
+}
+</script>
