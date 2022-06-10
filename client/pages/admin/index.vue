859a7e8a9c6c372d0316ba78e4baf7f981c5122e
Jawad Jahangir <jawadjahangir89@gmail.com>
2022-06-10 15:16:10 +0600

    delete and edit dialogs added to admin page user request table

--- client/pages/admin/index.vue
+++ client/pages/admin/index.vue
@@ -33,10 +33,83 @@
                   @click="dialog = true"
                   >Invite People</v-btn
                 > -->
+                <v-dialog
+                  v-model="dialog"
+                  max-width="500px"
+                >
+                  <v-card>
+                    <v-card-title>
+                      <span class="text-h5">Allocate storage</span>
+                    </v-card-title>
+
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12">
+                            <v-text-field
+                              v-if="editedItem.workspace"
+                              v-model="storage"
+                              label="Allocate"
+                              type="number"
+                              placeholder="10000"
+                              suffix="MB"
+                              outlined
+                              dense
+                            ></v-text-field>
+                            <v-card-text v-else>
+                              Cannot allocate storage to a user without a workspace
+                            </v-card-text>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
+
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="close"
+                      >
+                        Cancel
+                      </v-btn>
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="save"
+                      >
+                        Ok
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+                <v-dialog v-model="dialogDelete" max-width="500px">
+                  <v-card>
+                    <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                      <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                      <v-spacer></v-spacer>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
               </v-toolbar>
             </template>
-            <template v-slot:[`item.actions`]>
-              <v-icon color="error" class="ml-3" small> mdi-delete </v-icon>
+            <template v-slot:item.actions="{ item }">
+              <v-icon
+                small
+                class="mr-2"
+                @click="editItem(item)"
+              >
+                mdi-pencil
+              </v-icon>
+              <v-icon
+                small
+                @click="deleteItem(item)"
+              >
+                mdi-delete
+              </v-icon>
             </template>
           </v-data-table>
         </v-col>
@@ -47,7 +120,7 @@
 <script>
 import { v4 as uuid } from 'uuid'
 export default {
-  async fetch() {
+  async created() {
     const members = []
     try {
       const response = await this.$axios.get('/admin/waiting')
@@ -71,7 +144,10 @@ export default {
     }
   },
   data: () => ({
+    dialog: false,
+    dialogDelete: false,
     search: '',
+    storage: '',
     headers: [
       {
         text: 'Name',
@@ -81,18 +157,58 @@ export default {
       { text: 'Email', value: 'email' },
       { text: 'Workspace', value: 'workspace' },
       { text: 'Institute', value: 'institute' },
+      { text: 'Approve', value: 'actions', sortable: false },
     ],
     desserts: [],
-  }),
-  computed: {
-    dialog: {
-      get() {
-        return this.$store.getters.getWorkspaceInvitationDialog
-      },
-      set(newVal) {
-        this.$store.commit('setWorkspaceInvitationDialog', newVal)
-      },
+    editedIndex: -1,
+    editedItem: {
+      id: '',
+      name: '',
+      email: '',
+      institute: '',
+      workspace: null,
+      key: '',
     },
+  }),
+  methods: {
+    deleteItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        this.desserts.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+          this.storage = ''
+        })
+      },
+      save () {
+        console.log(storage)
+        console.log(this.desserts[this.editedIndex].workspace.name)
+
+        this.close()
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
   },
   layout: 'admin',
 }
